d536035b49c0016298dfcb038f0e477517f8b568
NathanielMether <nmet3@student.monash.edu>
2017-10-26 12:37:55 +1100

    Added editing of documents

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :documents, only: [:show]
+  resources :documents, only: [:show, :edit, :update]
   root 'documents#index'
   get '/documents' => redirect('/')
 end
