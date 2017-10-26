cfdf8e7940411fbf4be4ef48f55d7963a7bbd45d
NathanielMether <nmet3@student.monash.edu>
2017-10-26 12:50:47 +1100

    Refactor document controller with before_action

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,6 @@
 class DocumentsController < ApplicationController
+  before_action :set_document, only: [:show, :edit, :update]
+
   def index
     if signed_in?
       company = current_user.company
@@ -7,20 +9,12 @@ class DocumentsController < ApplicationController
   end
 
   def show
-    @document = Document.find(params[:id])
-    authorize @document
   end
 
   def edit
-    @document = Document.find(params[:id])
-    authorize @document
-    
   end
 
   def update 
-    @document = Document.find(params[:id])
-    authorize @document
-
     if @document.update(document_params)
       redirect_to @document
     else
@@ -31,5 +25,10 @@ class DocumentsController < ApplicationController
   private
     def document_params
       params.require(:document).permit(:name, :content)
+    end
+
+    def set_document
+      @document = Document.find(params[:id])
+      authorize @document
     end
 end
